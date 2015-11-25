96c72af3d5173b234527f3cd8bd275083920ee4d
xosauce <sergiu.ferentz@gmail.com>
2015-11-25 19:49:11 +0000

    Endpoint for traffic

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, render_template
+from decimal import *
 import json
 import awsdb
 from datetime import date, datetime
@@ -23,9 +24,47 @@ def json_date_handler(obj):
 
 @app.route("/req/db/pollution_data", methods=['GET'])
 def get_pollution_data():
-    print "Requesting data"
+    print "Requesting pollution data"
     cnx = awsdb.connect();
     result = awsdb.query(cnx, "SELECT * FROM pollution_table");
+    awsdb.close(cnx);
+    response = json.dumps(result, default=json_date_handler)
+    return response
+
+@app.route("/req/db/traffic_data/<lat>/<longi>", methods=['GET'])
+def get_traffic_data_with_param(lat, longi):
+    print "Requesting traffic data"
+    print lat, longi
+    lat_decimal = Decimal(repr(float(longi))).as_tuple().exponent
+    long_decimal = Decimal(repr(float(lat))).as_tuple().exponent
+    param2 = float(lat) + (10 ** int(lat_decimal))
+    params = []
+    if float(lat) < 0:
+        param2 = float(lat) - (10 ** int(lat_decimal))
+        params.append(param2)
+        params.append(float(lat))
+    else:
+        params.append(float(lat))
+        params.append(param2)
+    param4 = float(longi) + (10 ** int(long_decimal))
+    if float(longi) < 0:
+        param4 = float(longi) - (10 ** int(long_decimal))
+        params.append(param4)
+        params.append(float(longi))
+    else:
+        params.append(float(longi))
+        params.append(param4)
+    cnx = awsdb.connect();
+    result = awsdb.query(cnx, "SELECT * FROM traffic_london WHERE Latitude > %s AND Latitude < %s AND Longitude > %s AND Longitude < %s", params);
+    awsdb.close(cnx);
+    response = json.dumps(result, default=json_date_handler)
+    return response
+
+@app.route("/req/db/traffic_data", methods=['GET'])
+def get_traffic_data():
+    print "Requesting pollution data"
+    cnx = awsdb.connect();
+    result = awsdb.query(cnx, "SELECT * FROM traffic_london");
     awsdb.close(cnx);
     response = json.dumps(result, default=json_date_handler)
     return response
