3d524807844dc0e9b23114263c8a73361128162c
HugoDF <hdifrancesco@gmail.com>
2015-12-01 12:19:12 +0000

    refactor ajax queries

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,44 +2,80 @@ import React, { Component } from 'react';
 var Ajax = require('react-ajax');
 const ls = window.localStorage
 
+function ajax(url, cb){
+  var r = new XMLHttpRequest();
+  r.open("GET", url, true);
+  r.onreadystatechange = function () {
+    if (r.readyState != 4 || r.status != 200) return;
+    console.log("Success hitting: " + url);
+    cb(JSON.parse(r.responseText));
+  };
+  r.send();
+}
+
+var AjaxQueries = React.createClass({
+  render: function(){
+    if(this.props.pollution.length==0){
+      ajax("/req/db/pollution_data", this.props.storePollution);
+    }
+    if(this.props.londonProperties.length == 0){
+      ajax("/req/api/property_listings", this.props.storeLondonProperties);
+    }
+    return(
+      <span/>
+      )
+  }
+});
 var App = React.createClass({
   getInitialState: function(){
+    //initialise with empty object
     var obj = {
       pollution: [],
       geolocation: {
         postcode: null,
         lat: null,
         lon: null
-      }
+      },
+      londonProperties:[]
     }
-    if (window.localStorage.pollution != undefined){
+
+    //parse from localstorage if not undefined
+    if (ls.pollution != undefined){
       obj.pollution = JSON.parse(ls.pollution)
     }
-    if(window.localStorage.geolocation!= undefined){
+    if(ls.geolocation!= undefined){
       obj.geolocation = JSON.parse(ls.geolocation);
     }
-    
+    if(ls.londonProperties!=undefined){
+      obj.londonProperties = JSON.parse(ls.londonProperties)
+    }
     return obj;
   },
-  storePollution: function(err, data){
-    var someData = JSON.parse(data.text).slice(1,200);
-    this.setState({pollution: someData, displayPollution: someData});
-    ls.pollution = JSON.stringify(someData);
+  storePollution: function(data){
+    var someData = data.slice(1,200);
+    this.setState({pollution: data, displayPollution: data});
+    ls.pollution = JSON.stringify(data);
   },
   storeLocation: function(data){
     //Data has the form
     // {postcode,lat,lon}
     this.setState({geolocation: data});
     ls.geolocation = JSON.stringify(data);
+    ajax('/req/api/property_listings/' + data.lat +'/' + data.lon, this.storeLocationProperties);
+  },
+  storeLocationProperties: function(data){
+    console.log(data);
+    this.setState({locationProperties: data});
+    ls.locationProperties = JSON.stringify(data);
+  },
+  storeLondonProperties: function(data){
+    this.setState({londonProperties: data});
+    ls.londonProperties = JSON.stringify(data);
   },
   updateFilters: function(options){
     console.log(options);
   },
   render: function() {
-    var ajax =  '';
-    if(this.state.pollution.length==0){
-      ajax = <Ajax url="/req/db/pollution_data" onResponse={this.storePollution}/>;
-    }
     var _this = this;
     var children = React.Children.map(this.props.children, function(child) {
         return React.cloneElement(child, { 
@@ -50,7 +86,11 @@ var App = React.createClass({
     });
     return (
       <div>
-        {ajax}
+        <AjaxQueries 
+          londonProperties={this.state.londonProperties}
+          storeLondonProperties={this.storeLondonProperties}
+          pollution={this.state.pollution}
+          storePollution={this.storePollution}/>
         {children}
       </div>
     );
