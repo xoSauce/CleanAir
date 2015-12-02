d67a53217efd0e33e9ae08ac12bfd852bd6d1c52
Alexander Gamble <alexander.gamble@outlook.com>
2015-12-02 18:34:54 +0000

    fixed heatmap

--- frontend/src/components/GMaps.jsx
+++ frontend/src/components/GMaps.jsx
@@ -533,40 +533,38 @@ export default class GMaps extends React.Component {
 
 componentDidMount(){
 
-	 map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: {lat: 37.775, lng: -122.434},
-      disableDefaultUI: true,
-      zoomControl: true
-    });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 37.775, lng: -122.434},
+    disableDefaultUI: true,
+    zoomControl: true
+  });
 
-	 window.setTimeout(function(){
-	 	heatmap = new window.google.maps.visualization.HeatmapLayer({
-	    data: getPoints(),
-	    map: map
-	  });
-	 	heatmap.setMap(map);
+  // window.setTimeout(function(){
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: this.getPoints(),
+    map: map
+  });
+  heatmap.setMap(map);
 
-	 },2000 );
-	  
-
+	 // },2000 );
 	
-	var gradient = [
-	    'rgba(0, 255, 255, 0)',
-	    'rgba(0, 255, 255, 1)',
-	    'rgba(0, 191, 255, 1)',
-	    'rgba(0, 127, 255, 1)',
-	    'rgba(0, 63, 255, 1)',
-	    'rgba(0, 0, 255, 1)',
-	    'rgba(0, 0, 223, 1)',
-	    'rgba(0, 0, 191, 1)',
-	    'rgba(0, 0, 159, 1)',
-	    'rgba(0, 0, 127, 1)',
-	    'rgba(63, 0, 91, 1)',
-	    'rgba(127, 0, 63, 1)',
-	    'rgba(191, 0, 31, 1)',
-	    'rgba(255, 0, 0, 1)'
-	  ];
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ];
 
 	heatmap.set('radius', 20);
 	heatmap.set('gradient', gradient);
