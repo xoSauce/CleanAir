a0b987f66a5761657703b59b1d67f396a901f891
Paddy Fan <paddybalck@163.com>
2015-12-02 23:11:50 +0000

    Update content and styling for user and property modals

--- frontend/src/components/PropertyModal.jsx
+++ frontend/src/components/PropertyModal.jsx
@@ -15,31 +15,38 @@ export default class PropertyModal extends React.Component {
         <div className="modal-inner" onClick={this.stop}>
           <Link className="modal-close" to={"/map"}>Close</Link>
           <h3>Property Search</h3>
-          <div className="input-group">
-            <input type="checkbox"/><label>To buy</label>
-          </div>
-          <div className="input-group">
-            <input type="checkbox"/><label>To rent</label>
-          </div>
+            <div className="input-group-wrapper">
+              <div className="input-group">
+                <from>
+                  <input type="radio" name="property" onclick="check(this.value)" value="toBuy"/><label>&nbsp;&nbsp;To buy</label>
+                  &nbsp;&nbsp;<input type="radio" name="property" onclick="check(this.value)" value="toRent"/><label>&nbsp;&nbsp;To rent</label>
+                </from>
+              </div> 
+            </div>
           <hr/>
-          <h3>Property type</h3>
-          <div className="input-group">
-            <input type="checkbox"/><label>Houses</label>
-          </div>
-          <div className="input-group">
-            <input type="checkbox"/><label>Flats</label>
-          </div>
+          <h3>Property Type</h3>
+            <div className="input-group-wrapper">
+              <div className="input-group">
+                <from>
+                  <input type="radio" name="type" onclick="check(this.value)" value="Houses"/><label>&nbsp;&nbsp;Houses</label>
+                  &nbsp;&nbsp;<input type="radio" name="type" onclick="check(this.value)" value="Flats"/><label>&nbsp;&nbsp;Flats</label>
+                </from>
+              </div> 
+            </div>
           <hr/>
           <h3>Price Range</h3>
-          <div className="input-group">
-            <label>&pound;</label><input type="number"/><input type="number"/>
+          <div className="input-group-wrapper">
+            <div className="input-group">
+              <label>&pound;&nbsp;</label><input type="number"/><label>&nbsp;to&nbsp;</label><input type="number"/>
+            </div>
           </div>
           <hr/>
           <h3>Number of bedrooms</h3>
-          <div className="input-group">
-            <input type="number"/>
+          <div className="input-group-wrapper">
+            <div className="input-group">
+              <input type="number"/>
+            </div>
           </div>
-
 
         </div>
       </div>
